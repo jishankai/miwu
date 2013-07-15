be27d4d46d6f57f17a736596e3e69948e2d766e2
skji <jishankai@gmail.com>
2013-07-16 00:38:16 +0800

    初始化代码

---
+++ miwu/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  miwuAppController.h
+//  miwu
+//
+//  Created by Ji Shankai on 13-7-16.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
